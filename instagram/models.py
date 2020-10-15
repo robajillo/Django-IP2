6a4713539bbaa0050888f3fac1482843acdc07d5
robajillo <rjillo@yahoo.com>
2020-10-15 21:51:38 +0300

    created class post

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(blank=true,null=true)
+    author = models.ForeignKey('auth.User',on_delete = models.CASCADE)
+    caption = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    
