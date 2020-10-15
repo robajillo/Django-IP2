74cb1982154ac4e0a4248d3101737e2602df4b46
robajillo <rjillo@yahoo.com>
2020-10-15 21:59:00 +0300

    imported datetime

--- instagram/models.py
+++ instagram/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+import datetime
 
 class Post(models.Model):
-    image = models.ImageField(blank=true,null=true)
+    image = models.ImageField(blank=True,null=True)
     author = models.ForeignKey('auth.User',on_delete = models.CASCADE)
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
