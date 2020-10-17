2f2adca3ea8af2b5ebf2394f9ba552f045bfe541
robajillo <rjillo@yahoo.com>
2020-10-17 20:39:13 +0300

    updated settings file

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -96,12 +96,7 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
