5df860ce432e6f33ee810b438b0d0d6910e3ebc0
robajillo <rjillo@yahoo.com>
2020-10-17 20:48:22 +0300

    defined a view function for profile

--- instagram/views.py
+++ instagram/views.py
@@ -36,3 +36,19 @@ def index(request):
         form = PostForm()
     
     return render(request, 'index.html')
+
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateUserProfileForm(instance=request.user.profile)
+    
+    return render(request, 'profile.html')
