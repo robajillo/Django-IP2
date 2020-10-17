b516e29026af0788256ee112aefc80127ec7c6ee
robajillo <rjillo@yahoo.com>
2020-10-17 18:14:45 +0300

    defined view function for signup

--- instagram/views.py
+++ instagram/views.py
@@ -4,3 +4,16 @@ from django.contrib.auth.decorators import login_required
 from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
