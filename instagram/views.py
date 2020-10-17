a9cec8df15717d7fc296e01c33aaf738da60bd6e
robajillo <rjillo@yahoo.com>
2020-10-17 18:11:39 +0300

    imported User

--- instagram/views.py
+++ instagram/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, redirect
 from . models import Post
 from django.contrib.auth.decorators import login_required
 from .models import Post, Comment, Profile, Follow
+from django.contrib.auth.models import User
 
