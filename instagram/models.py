5d641c400993a2ab96843747e75353a03c7a8480
robajillo <rjillo@yahoo.com>
2020-10-17 17:27:10 +0300

    created class Profile

--- instagram/models.py
+++ instagram/models.py
@@ -9,4 +9,16 @@ class Post(models.Model):
     name = models.CharField(max_length=250, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+
+    def __str__(self):
+        return f'{self.user.name} Post'
     
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(blank=True,null=True)
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
