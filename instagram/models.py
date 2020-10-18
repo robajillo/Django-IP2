3b0ebc8030569213e9159b48f5bcf0f59fd89c7f
robajillo <rjillo@yahoo.com>
2020-10-18 13:25:43 +0300

    modified file

--- instagram/models.py
+++ instagram/models.py
@@ -2,6 +2,9 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django_currentuser.db.models import CurrentUserField
+from django.db.models.signals import post_save
+
 
 class Post(models.Model):
     image = models.ImageField(upload_to='images/')
@@ -9,13 +12,29 @@ class Post(models.Model):
     caption = models.CharField(max_length=250)
     name = models.CharField(max_length=250, default='')
     created_date = models.DateTimeField(default=timezone.now)
-    likes = models.ManyToManyField(User, related_name='likes' )
+    # likes = models.ManyToManyField(User, related_name='likes' )
+
 
     def __str__(self):
-        return f'{self.user.name} Post'
+        return self.name
     
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        prof = cls.objects.filter(name__name__icontains=search_term)
+        return prof
+        
+    def delete_image(self):
+        self.delete()
+    
+    @classmethod
+    def update_caption(cls, id, value):
+        cls.objects.filter(id=id).update(caption=value)
+
 class Profile(models.Model):
-    user =models.CharField(max_length=255)
+    user =models.ForeignKey(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='images/')
     bio = models.TextField(max_length=500, default="My Bio")
     name = models.CharField( max_length=120)
