3c130f05618e35c4673b37ed36f1a4239371b441
robajillo <rjillo@yahoo.com>
2020-10-21 10:53:45 +0300

    deleted and created environment

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/roba/Desktop/moringa-school-projects/python/instagram/virtual"
+set -gx VIRTUAL_ENV "/home/roba/Desktop/moringa-school-projects/python/Instagram/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
