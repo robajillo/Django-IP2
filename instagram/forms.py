3a9ab825eaf83518fd0792e84b79a35d7dd73d9b
robajillo <rjillo@yahoo.com>
2020-10-22 11:58:05 +0300

    --

--- instagram/forms.py
+++ instagram/forms.py
@@ -4,7 +4,7 @@ from .models import Profile, Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 
 
-class RegistrationForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
